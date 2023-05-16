b83c228b29784f52636504c88e6c9f51ffe1d74f
Kristopher Gray <kristopher.gray99@gmail.com>
2023-05-16 19:09:53 +0200

    basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './assets/scss/App.scss'
 
 const App = () => {
+
+
 	return (
-		<div className='container'>
-			hej
+		<div className='App container'>
+			<form id='login-form' className='mt-5'>
+				<label htmlFor="email">Email</label>
+				<input type="text" id='email' />
+
+				<label htmlFor="password">Password</label>
+				<input type="text" id='password' />
+
+				<button className='btn btn-success' type='submit'>Log in</button>
+			</form>
 		</div>
 	)
 }
