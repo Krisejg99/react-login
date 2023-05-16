f66a3e33c8450f901b8dac0f5032e2ce24cf97bf
Kristopher Gray <kristopher.gray99@gmail.com>
2023-05-16 19:52:10 +0200

    user state accepts changes in input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
+import { useState } from 'react'
 import './assets/scss/App.scss'
 
-const App = () => {
+type User = {
+	email: string
+	password: string
+}
 
+const App = () => {
+	// user state
+	const [user, setUser] = useState<User>({ email: '', password: '' })
+
+	// when user submits form update state for 'user'
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault()
+
+		// reset input fields
+		setUser({ email: '', password: '' })
+	}
 
 	return (
 		<div className='App container'>
-			<form id='login-form' className='mt-5'>
-				<label htmlFor="email">Email</label>
-				<input type="text" id='email' />
+			<form
+				id='login-form'
+				className='mt-5'
+				onSubmit={handleSubmit}
+			>
+				<label htmlFor='email'>Email</label>
+				<input
+					type='email'
+					id='email'
+					onChange={e => setUser({ ...user, email: e.target.value })}
+					value={user.email}
+				/>
 
-				<label htmlFor="password">Password</label>
-				<input type="text" id='password' />
+				<label htmlFor='password'>Password</label>
+				<input
+					type='password'
+					id='password'
+					onChange={e => setUser({ ...user, password: e.target.value })}
+					value={user.password}
+				/>
 
 				<button className='btn btn-success' type='submit'>Log in</button>
 			</form>
