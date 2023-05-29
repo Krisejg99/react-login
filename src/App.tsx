086fbd49e2a7d6dffe7d81b778d8bb922b12b685
Kristopher Gray <kristopher.gray99@gmail.com>
2023-05-29 17:23:04 +0200

    file should have gone with last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,65 @@
 import { useState } from 'react'
 import './assets/scss/App.scss'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { getMovies } from './services/MovieAPI'
+import { Movie } from './types'
 
 type User = {
-	email: string
+	username: string
 	password: string
 }
 
+const queryClient = new QueryClient()
+
 const App = () => {
 	// user state
-	const [user, setUser] = useState<User>({ email: '', password: '' })
+	const [user, setUser] = useState<User>({ username: '', password: '' })
+	const [loggedIn, setLoggedIn] = useState(false)
+	const [movies, setMovies] = useState<Movie[]>([])
 
 	// when user submits form update state for 'user'
-	const handleSubmit = (e: React.FormEvent) => {
+	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault()
 
-		// reset input fields
-		setUser({ email: '', password: '' })
+		const movies = await getMovies()
+		setMovies(movies)
+
+		setLoggedIn(true)
 	}
 
 	return (
-		<div className='App container'>
-			<form
-				id='login-form'
-				className='mt-5'
-				onSubmit={handleSubmit}
-			>
-				<label htmlFor='email'>Email</label>
-				<input
-					type='email'
-					id='email'
-					onChange={e => setUser({ ...user, email: e.target.value })}
-					value={user.email}
-				/>
+		<QueryClientProvider client={queryClient}>
+			<div className='App container'>
+				{!loggedIn
+					? (
+						<form
+							id='login-form'
+							className='mt-5'
+							onSubmit={handleSubmit}
+						>
+							<label htmlFor='username'>Email</label>
+							<input
+								type='text'
+								id='username'
+								onChange={e => setUser({ ...user, username: e.target.value })}
+								value={user.username}
+							/>
 
-				<label htmlFor='password'>Password</label>
-				<input
-					type='password'
-					id='password'
-					onChange={e => setUser({ ...user, password: e.target.value })}
-					value={user.password}
-				/>
+							<label htmlFor='password'>Password</label>
+							<input
+								type='password'
+								id='password'
+								onChange={e => setUser({ ...user, password: e.target.value })}
+								value={user.password}
+							/>
 
-				<button className='btn btn-success' type='submit'>Log in</button>
-			</form>
-		</div>
+							<button className='btn btn-success' type='submit'>Log in</button>
+						</form>
+					)
+					: <div>WELCOME {user.username}</div>
+				}
+			</div>
+		</QueryClientProvider>
 	)
 }
 
