d1c5d9a48370eecccf1b9ce724d75f8cfb451af1
Kristopher Gray <kristopher.gray99@gmail.com>
2023-05-29 17:22:39 +0200

    starting with react-query, loggedIn and movies state,

---
+++ src/services/MovieAPI.ts
@@ -0,0 +1,12 @@
+import axios from "axios"
+import { Movie } from '../types'
+
+const BASE_URL = 'http://localhost:3000'
+
+/**
+ * Get all movies
+ */
+export const getMovies = async () => {
+	const res = await axios.get(`${BASE_URL}/movies`)
+	return res.data as Movie[]
+}
