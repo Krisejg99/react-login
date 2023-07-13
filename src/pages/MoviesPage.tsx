3ac65d57777acef623a2c51a840d8c6227a780c3
Kristopher Gray <kristopher.gray99@gmail.com>
2023-07-13 17:28:08 +0200

    created data and columns to send in to the useReactTable to create a table

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,11 +1,39 @@
-import MovieList from "../components/MovieList"
+import { useReactTable, ColumnDef, getCoreRowModel } from '@tanstack/react-table'
+import { useMemo } from 'react'
+import defaultMovies from '../../data/defaultMovies'
+import { Movie } from '../types'
 
 const MoviesPage = () => {
+	const data = useMemo(() => defaultMovies, [])
+
+	const columns: ColumnDef<Movie>[] = [
+		{
+			header: 'ID',
+			accessorKey: 'id',
+		},
+		{
+			header: 'Title',
+			accessorKey: 'title',
+		},
+		{
+			header: 'Realse Year',
+			accessorKey: 'release_year',
+		},
+		{
+			header: 'Watched',
+			accessorKey: 'watched',
+		},
+	]
+
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel()
+	})
+
 	return (
 		<>
-			<h1>My movie list</h1>
 
-			<MovieList />
 		</>
 	)
 }
