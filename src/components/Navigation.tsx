6fd980976a59b577477ff1aba6a10012e6a3fea9
Kristopher Gray <kristopher.gray99@gmail.com>
2023-07-29 12:33:32 +0200

    navbar says Profile if logged in

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,8 +2,11 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { NavLink } from 'react-router-dom'
+import useLoginContext from '../hooks/useLoginContext'
 
 const Navigation = () => {
+	const { login } = useLoginContext()
+
 	return (
 		<Navbar expand="sm" className="bg-body-tertiary">
 			<Container>
@@ -12,7 +15,8 @@ const Navigation = () => {
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
 						<Nav.Link as={NavLink} to="/movies">Movies</Nav.Link>
-						<Nav.Link as={NavLink} to="/login">Login</Nav.Link>
+
+						<Nav.Link as={NavLink} to="/login">{!login ? <>Login</> : <>Profile</>}</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
