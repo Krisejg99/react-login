5b6bceebf578e15d1ca5d2346954e7deda1a9d73
Kristopher Gray <kristopher.gray99@gmail.com>
2024-02-27 10:43:11 +0100

    Added next app

---
+++ next/src/app/publicSite/[organization]/[plant]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+	return <div>PLANT</div>;
+};
+
+export default page;
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,42 @@
-import { Container } from 'react-bootstrap'
-import './assets/scss/App.scss'
-import { Route, Routes } from 'react-router-dom'
-import NotFound from './pages/NotFound'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import MoviesPage from './pages/MoviesPage'
-import Navigation from './components/Navigation'
+import { Container } from "react-bootstrap";
+import "./assets/scss/App.scss";
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import MoviesPage from "./pages/MoviesPage";
+import Navigation from "./components/Navigation";
 
 const App = () => {
 	return (
-		<div id='App' className='bg-dark text-white'>
+		<div id="App" className="bg-dark text-white">
 			<Navigation />
 
-			<Container className='py-3'>
+			<Container className="py-3">
 				<Routes>
-					<Route path='/' element={<MoviesPage />} />
-					<Route path='/movies' element={<MoviesPage />} />
-					<Route path='/login' element={<LoginPage />} />
-					<Route path='/register' element={<RegisterPage />} />
+					<Route path="/" element={<MoviesPage />} />
+					<Route path="/movies" element={<MoviesPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/register" element={<RegisterPage />} />
 
-					<Route path='/*' element={<NotFound />} />
+					<Route path="/*" element={<NotFound />} />
 				</Routes>
 			</Container>
-		</div>
-	)
-}
 
-export default App
+			<div className="p-2">
+				<div
+					hx-get="http://localhost:3000/htmx/widget"
+					// hx-trigger="click"
+					// hx-target="#widget-content"
+					// hx-swap="innerHTML"
+				>
+					Get data
+				</div>
+
+				<div id="widget-content">empty</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
